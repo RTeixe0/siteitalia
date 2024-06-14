3f85f11c8ed31893a1fd6792dc1b202bc81204cc
Renan Teixeira <126295997+RTeixe0@users.noreply.github.com>
2024-06-14 17:19:54 -0300

    Foi adicionado classes database e usuario

---
+++ classes/usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+class User {
+    private $id;
+    private $username;
+    private $password;
+    private $email;
+
+    public function __construct($username, $password, $email) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+    }
+
+    // Getters and setters
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+}
+
+?>
